365cb85aac449a36b2b96ab17d9e639491bb9f7b
Wojciech Korban <wojtasck1@o2.pl>
2017-08-17 10:13:09 +0200

    mt table working

--- src/app/users/users-tab/users-tab.component.ts
+++ src/app/users/users-tab/users-tab.component.ts
@@ -1,12 +1,11 @@
-
 import {Component, ViewChild} from '@angular/core';
-import {User} from '../shared/user.model';
-import {UserService} from '../shared/user.service';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {MdDialog} from '@angular/material';
-import {AppConfig} from '../../config/app.config';
-import {Router} from '@angular/router';
-import {LoggerService} from '../../core/logger.service';
+import {DataSource} from '@angular/cdk';
+import {MdSort} from '@angular/material';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-users-tab',
@@ -14,57 +13,110 @@ import {LoggerService} from '../../core/logger.service';
   styleUrls: ['./users-tab.component.css']
 })
 export class UsersTabComponent {
+  displayedColumns = ['userId', 'userName', 'progress', 'color'];
+  exampleDatabase = new ExampleDatabase();
+  dataSource: ExampleDataSource | null;
 
-  public users: User[];
+  @ViewChild(MdSort) sort: MdSort;
 
-  constructor(
-    private userService: UserService, 
-    private dialog: MdDialog, 
-    private ruter: Router,
-    private FormBuilder: FormBuilder) {
-    this.userService.getAllUsers()
+  ngOnInit() {
+    this.dataSource = new ExampleDataSource(this.exampleDatabase, this.sort);
+  }
+}
+
+/** Constants used to fill up our data base. */
+const COLORS = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
+  'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
+const NAMES = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
+  'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
+  'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
+
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  color: string;
+}
+
+/** An example database that the data source uses to retrieve data for the table. */
+export class ExampleDatabase {
+  /** Stream that emits whenever the data has been modified. */
+  dataChange: BehaviorSubject<UserData[]> = new BehaviorSubject<UserData[]>([]);
+  get data(): UserData[] { return this.dataChange.value; }
+
+  constructor() {
+    // Fill up the database with 100 users.
+    for (let i = 0; i < 100; i++) { this.addUser(); }
   }
 
-  ///////////////////////////////////////////////
-  title = 'Angular 2 CRUD operation with an Array';
-  employees = [
-    { name: "Sikandar", position: "Programmer" },
-    { name: "Alex", position: "Designer" },
-    { name: "Maria", position: "Manager" }
-  ];
-  model: any = {};
-  model2: any = {};
-  msg: any = "";
-  addEmployee() {
-    this.employees.push(this.model);
-    this.model = {};
-    this.msg = "Record is successfully added..... ";
-
+  /** Adds a new user to the database. */
+  addUser() {
+    const copiedData = this.data.slice();
+    copiedData.push(this.createNewUser());
+    this.dataChange.next(copiedData);
   }
-  deleteEmployee(i) {
-    this.employees.splice(i, 1);
-    this.msg = "Record is successfully deleted..... "; 
+
+  /** Builds and returns a new User. */
+  private createNewUser() {
+    const name =
+        NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+        NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+
+    return {
+      id: (this.data.length + 1).toString(),
+      name: name,
+      progress: Math.round(Math.random() * 100).toString(),
+      color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+    };
+  }
+}
+
+/**
+ * Data source to provide what data should be rendered in the table. Note that the data source
+ * can retrieve its data in any way. In this case, the data source is provided a reference
+ * to a common data base, ExampleDatabase. It is not the data source's responsibility to manage
+ * the underlying data. Instead, it only needs to take the data and send the table exactly what
+ * should be rendered.
+ */
+export class ExampleDataSource extends DataSource<any> {
+  constructor(private _exampleDatabase: ExampleDatabase, private _sort: MdSort) {
+    super();
+  }
+
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<UserData[]> {
+    const displayDataChanges = [
+      this._exampleDatabase.dataChange,
+      this._sort.mdSortChange,
+    ];
+
+    return Observable.merge(...displayDataChanges).map(() => {
+      return this.getSortedData();
+    });
+  }
+
+  disconnect() {}
+
+  /** Returns a sorted copy of the database data. */
+  getSortedData(): UserData[] {
+    const data = this._exampleDatabase.data.slice();
+    if (!this._sort.active || this._sort.direction == '') { return data; }
  
-  }
-  myValue;
-  editEmployee(k) {
-    this.model2.name = this.employees[k].name;
-    this.model2.position = this.employees[k].position;
-    this.myValue = k;
+    return data.sort((a, b) => {
+      let propertyA: number|string = '';
+      let propertyB: number|string = '';
 
-  }
-  updateEmployee() {
-    let k = this.myValue;
-    for (let i = 0; i < this.employees.length; i++) {
-      if (i == k) {
-        this.employees[i] = this.model2;
-        this.model2 = {};
-        this.msg = "Record is successfully updated..... ";
+      switch (this._sort.active) {
+        case 'userId': [propertyA, propertyB] = [a.id, b.id]; break;
+        case 'userName': [propertyA, propertyB] = [a.name, b.name]; break;
+        case 'progress': [propertyA, propertyB] = [a.progress, b.progress]; break;
+        case 'color': [propertyA, propertyB] = [a.color, b.color]; break;
       }
+ 
+      let valueA = isNaN(+propertyA) ? propertyA : +propertyA;
+      let valueB = isNaN(+propertyB) ? propertyB : +propertyB;
 
-    }
-  }
-  clickMe() {
-    this.msg = "";
+      return (valueA < valueB ? -1 : 1) * (this._sort.direction == 'asc' ? 1 : -1);
+    });
   }
 }
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,35 +1,43 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { SharedModule } from '../shared/modules/shared.module';
 
 import { UsersComponent } from './users.component';
-import { UserDetailComponent} from './user-detail/user-detail.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 import { UsersTopComponent } from './users-top/users-top.component';
 import { UsersTabComponent } from './users-tab/users-tab.component';
-import { UserService} from './shared/user.service';
+import { UserService } from './shared/user.service';
 import { HttpModule } from '@angular/http';
+import { DataSource } from '@angular/cdk';
+import { MdTableModule } from '@angular/material';
+import { MdSortModule , MdIconModule } from '@angular/material';
+import { CdkTableModule } from '@angular/cdk';
+
 
 @NgModule({
   imports: [
     UsersRoutingModule,
     FormsModule,
-    // BrowserModule,
-    CommonModule
+    CommonModule,
+    MdTableModule,
+    CdkTableModule,
+    MdSortModule,
+    MdIconModule,
   ],
   declarations: [
-    UsersComponent,  
-    UsersTopComponent,  
-    UserDetailComponent,   
+    UsersComponent,
+    UsersTopComponent,
+    UserDetailComponent,
     UsersTabComponent,
   ],
   bootstrap: [UsersTabComponent],
-   providers: [
-     UserService 
-   ]
+  providers: [
+    UserService
+  ]
 })
 
 export class UsersModule { }
