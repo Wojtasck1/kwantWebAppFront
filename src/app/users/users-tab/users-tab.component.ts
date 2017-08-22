1757c39ec5f6331f14a2a22036b5a7b56d2708fc
Wojciech Korban <wojtasck1@o2.pl>
2017-08-22 10:35:06 +0200

    getAllUsers

--- src/app/users/users-tab/users-tab.component.ts
+++ src/app/users/users-tab/users-tab.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataSource } from '@angular/cdk';
-import { MdPaginator, MdSelect } from '@angular/material';
+import { MdPaginator, MdSelect, MdSort } from '@angular/material';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import { User } from './../shared/user.model';
@@ -17,88 +17,87 @@ import 'rxjs/add/operator/map';
 })
 export class UsersTabComponent implements OnInit {
   displayedColumns = ['userId', 'name', 'surname', 'email', 'actions'];
-  usersDatabase = new UsersDatabase(this.userService);
-  public dataSource: UsersDataSource | any;
-  public users: User[];
-  user: User;
-  //userService: UserService;
+  userService: UserService;
+  exampleDatabase = new ExampleDatabase(this.userService);
+  dataSource: ExampleDataSource | null;
 
   @ViewChild(MdPaginator) paginator: MdPaginator;
-
-  constructor(private userService: UserService) {
-    this.userService.getAllUsers().subscribe((users) => {
-      this.users = users.sort((a, b) => {
-        return b.user_id - a.user_id;
-      });
-    });
-
-  }
+  @ViewChild(MdSort) sort: MdSort;
 
   ngOnInit() {
-    this.dataSource = new UsersDataSource(this.usersDatabase, this.paginator);
-    console.log(this.users);
-    // console.log(this.dataSource);
-    //this.userService.getAllUsers();
+    this.dataSource = new ExampleDataSource(this.exampleDatabase, this.paginator, this.sort);
   }
-
-  public onEditClick() {
-    console.log(this.users);
-  };
-
-  public onDescClick() {
-    console.log("onDescClick");
-  };
-  public onDeleteClick() {
-    console.log("onDElClick");
-  };
 }
 
+export interface attendanceData {
+  userId: number;
+  name: string;
+  surname: string;
+  email: string;
+}
+
+
 /** An example database that the data source uses to retrieve data for the table. */
-export class UsersDatabase {
+export class ExampleDatabase {
   /** Stream that emits whenever the data has been modified. */
-  dataChange: BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
-  get data(): User[] { return this.dataChange.value; }
+  public dataChange: BehaviorSubject<attendanceData[]> = new BehaviorSubject<attendanceData[]>([]);
+  get data(): attendanceData[] { return this.dataChange.value; }
 
-  private _userService: UserService;
+  //public userService: UserService;
 
-  // public getAllUsers() {
-  //   return this._userService.getAllUsers();
-  // } 
-
-  constructor(userService: UserService) {
+  constructor(public userService: UserService) {
     userService.getAllUsers().subscribe(data => this.dataChange.next(data));
   }
 
-  // ngOnInit() {
-  //   this.getAllUsers();
-  // }
 }
 
-/**
- * Data source to provide what data should be rendered in the table. Note that the data source
- * can retrieve its data in any way. In this case, the data source is provided a reference
- * to a common data base, ExampleDatabase. It is not the data source's responsibility to manage
- * the underlying data. Instead, it only needs to take the data and send the table exactly what
- * should be rendered.
- */
-export class UsersDataSource extends DataSource<User> {
-  constructor(private _usersDatabase: UsersDatabase, private _paginator: MdPaginator) {
+export class ExampleDataSource extends DataSource<any> {
+  _filterChange = new BehaviorSubject('');
+  get filter(): string { return this._filterChange.value; }
+  set filter(filter: string) { this._filterChange.next(filter); }
+
+  constructor(private _exampleDatabase: ExampleDatabase, private _paginator: MdPaginator, private _sort: MdSort) {
     super();
   }
 
-  connect(): Observable<User[]> {
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<attendanceData[]> {
     const displayDataChanges = [
-      this._usersDatabase.dataChange,
-      this._paginator.page
+      this._exampleDatabase.dataChange,
+      this._paginator.page,
+      this._sort.mdSortChange
     ];
 
-    return Observable.merge(...displayDataChanges).map((data, page) => {
-      const clonedData = data.slice();
-
+    return Observable.merge(...displayDataChanges).map(() => {
+      // const data = this._exampleDatabase.data.slice();
+      const data = this.getSortedData();
+      // Grab the page's slice of data.
       const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
       return data.splice(startIndex, this._paginator.pageSize);
-    })
+    });
   }
+
   disconnect() { }
-  
+
+  getSortedData(): attendanceData[] {
+    const data = this._exampleDatabase.data.slice();
+    if (!this._sort.active || this._sort.direction == '') { return data; }
+
+    return data.sort((a, b) => {
+      let propertyA: number | string = '';
+      let propertyB: number | string = '';
+
+      switch (this._sort.active) {
+        case 'userId': [propertyA, propertyB] = [a.userId, b.userId]; break;
+        case 'name': [propertyA, propertyB] = [a.name, b.name]; break;
+        case 'surname': [propertyA, propertyB] = [a.surname, b.surname]; break;
+        case 'email': [propertyA, propertyB] = [a.email, b.email]; break;
+      }
+
+      let valueA = isNaN(+propertyA) ? propertyA : +propertyA;
+      let valueB = isNaN(+propertyB) ? propertyB : +propertyB;
+
+      return (valueA < valueB ? -1 : 1) * (this._sort.direction == 'asc' ? 1 : -1);
+    });
+  }
 }
