4f9c8d9b1c450004e38379e3a941d8e9c49a6772
Wojciech Korban <wojtasck1@o2.pl>
2017-08-30 11:05:18 +0200

    add users list

--- src/app/users/users-tab/users-tab.component.ts
+++ src/app/users/users-tab/users-tab.component.ts
@@ -1,19 +1,40 @@
 import { Component, OnInit} from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { User } from './../shared/user.model';
+import { UserService } from './../shared/user.service';
+import { MdProgressSpinnerModule } from '@angular/material';
+import { Router } from '@angular/router';
+import { AppConfig } from '../../config/app.config';
 
 
 @Component({
   selector: 'app-users-tab',
   templateUrl: './users-tab.component.html',
   styleUrls: ['./users-tab.component.css'],
-  // providers: [UserService]
 })
 
 export class UsersTabComponent implements OnInit {
 
-  constructor(){
+  users: User[];
+
+  constructor(private userService: UserService,
+              private router: Router){
+    userService.getAllUsers().subscribe((users) => {
+      this.users = users.sort((a,b) =>{
+        return b.userId - a.userId;
+      });
+    });
   } 
 
+  load(){
+    console.log(this.users);
+  }
+
   ngOnInit(){
+    console.log(this.users);
+  }
+
+  seeUsersDetails(user){
+    this.router.navigate([AppConfig.routes.users + '/' + user.userId]);
   }
 }
